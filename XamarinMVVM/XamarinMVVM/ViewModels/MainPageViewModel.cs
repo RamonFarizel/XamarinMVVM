7555d738fccd5e0605d4920f8fd139cdfad9fe90
Farizel <ramonfarizel@gmail.com>
2019-07-11 21:47:55 -0300

    Primeiros testes MVVM

---
+++ XamarinMVVM/XamarinMVVM/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamarinMVVM.ViewModels.Base;
+
+namespace XamarinMVVM.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private string _nome = string.Empty;
+        public string Nome
+        {
+            get { return _nome; }
+            set {SetProperty(ref(_nome), value); }
+        }
+
+        public ICommand AlterarNome { get; set; }
+
+       public MainPageViewModel()
+        {
+            Nome = "Ramon";
+            AlterarNome = new Command(AlterandoNome);
+        }
+
+        private void AlterandoNome()
+        {
+            Nome = "Ramon";
+        }
+    }
+}
